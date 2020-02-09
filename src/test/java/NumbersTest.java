de9859560015bcff90235a08f270ee885d2d66b2
Farrukh Karimov <Karimov.farrux@list.ru>
2020-02-09 14:50:19 +0300

    new mvn initiation, and add tests

---
+++ src/test/java/NumbersTest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NumbersTest {
+    @Test
+    public void sumCorrectForPositiveNumbers(){
+        Numbers numbers = new Numbers();
+        int a = 10;
+        int b = 353;
+        int result = numbers.summ(a, b);
+        assertEquals(a + b, result);
+    }
+
+    @Test
+    public void sumCorrectForNegativeNumbers(){
+        Numbers numbers = new Numbers();
+        int a = -245;
+        int b = -4624;
+        int result = numbers.summ(a, b);
+        assertEquals(a + b, result);
+    }
+
+    @Test
+    public void intervalSumOk(){
+        Numbers numbers = new Numbers();
+        int a = -10;
+        int b = 253;
+        int correctSum = 0;
+        for(int i = a; i <= b; i++){
+            correctSum += i;
+        }
+        int functionCountedSum = numbers.intervalSumm(a, b);
+        assertEquals(correctSum, functionCountedSum);
+    }
+}
