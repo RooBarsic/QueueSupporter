e1e475a94ed0d31590f235fe7526d8067dedd801
Farrukh Karimov <Karimov.farrux@list.ru>
2020-02-23 20:35:51 +0300

    refactor by ConcurrentSkipListSet

--- src/main/java/logic/queue/EngineeredQueue.java
+++ src/main/java/logic/queue/EngineeredQueue.java
@@ -3,10 +3,8 @@ package logic.queue;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 import javax.swing.Timer;
 
 /**
@@ -15,12 +13,13 @@ import javax.swing.Timer;
  * Modification Date: 12.02.2020
  */
 public class EngineeredQueue<T>{
+    private int idCounter = 0;
     private int taskExecutionDelayMs = 3 * 60 * 1000;
     private Timer releasingTimer;
     @NotNull
     private final String queueName;
-    private final List<T> customersList = new ArrayList<>();
-    private final Set<T> customersSet = new HashSet<>();
+    private final ConcurrentHashMap<T, Integer> customerIdByCustomer = new ConcurrentHashMap<>();
+    private final ConcurrentSkipListSet<Integer> orderedIds = new ConcurrentSkipListSet<>();
 
     /**
      * Конструктор - создание новой очереди с определенным именем и дефолтной задержкой на обслуживании
@@ -43,13 +42,9 @@ public class EngineeredQueue<T>{
 
     private void initTimer(){
         releasingTimer = new Timer(taskExecutionDelayMs, actionEvent -> {
-            synchronized (customersList){
-                if(customersList.size() > 0){
-                    synchronized (customersSet){
-                        remove(customersList.get(0));
-                    }
-                }
-            }
+            orderedIds.remove(
+                    orderedIds.first()
+            );
         });
     }
 
@@ -58,15 +53,23 @@ public class EngineeredQueue<T>{
         return queueName;
     }
 
+    public boolean contains(T var){
+        return orderedIds.contains(
+                customerIdByCustomer.getOrDefault(var, 0)
+        );
+    }
+
     /**
      * Метод для добавления обекта в очередь
      * @param var принимает на вход обект
      * @return возвращает true если элемент успешно добавлен, false если такой елемент уже существует
      */
     public boolean add(@NotNull final T var){
-        if(customersSet.add(var)){
-            customersList.add(var);
-            if(customersList.size() == 1){
+        if(!contains(var)){
+            idCounter++;
+            customerIdByCustomer.put(var, idCounter);
+            orderedIds.add(idCounter);
+            if(orderedIds.size() == 1){
                 releasingTimer.start();
             }
             return true;
@@ -75,18 +78,21 @@ public class EngineeredQueue<T>{
     }
 
     /**
-     * :TODO оптимизировать время работы, сейчай O(N)
      * Метод для удаления обекта из очереди
      * @param var принимает на вход обект
      * @return возвращает true если такой обект был в очереди и мы только что его удалили, false иначе
      */
     public boolean remove(@NotNull final T var){
-        if(customersSet.remove(var)){
-            final int varIndex = customersList.indexOf(var);
-            customersList.remove(varIndex);
-            if(customersSet.size() == 0){
+        if(contains(var)){
+            final int varIndex = orderedIds
+                    .headSet(
+                            customerIdByCustomer.get(var),
+                            true
+                    ).size();
+            orderedIds.remove(customerIdByCustomer.get(var));
+            if(orderedIds.size() == 0){
                 releasingTimer.stop();
-            } else if(varIndex == 0){
+            } else if(varIndex == 1){
                 releasingTimer.restart();
             }
             return true;
@@ -95,30 +101,29 @@ public class EngineeredQueue<T>{
     }
 
     /**
-     * :TODO оптимизировать время работы, сейчай O(N)
      * Метод для поиска позиции обекта в очереди
      * @param var принимает на вход обект
      * @return возвращает позицию обекта в очереди - если в очереди есть такой обект ( иначе возвращает -1 )
      */
     public int findIndex(@NotNull final T var){
-        if(customersSet.contains(var)){
-            return customersList.indexOf(var) + 1;
-        }
-        return -1;
+        return orderedIds
+                .headSet(
+                        customerIdByCustomer.getOrDefault(var, 0),
+                        true
+                ).size() - 1;
     }
 
     /**
      * @return возвращает количество элементов в очереди
      */
     public int size(){
-        return customersList.size();
+        return orderedIds.size();
     }
 
     /**
      * Метод для очистки очереди
      */
     public void clear(){
-        customersList.clear();
-        customersSet.clear();
+        orderedIds.clear();
     }
 }
